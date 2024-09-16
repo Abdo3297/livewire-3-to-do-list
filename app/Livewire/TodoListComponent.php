3e78a4a7dac15331bb8109fbb2ca08c2030d08c1
Abdo3297 <abdelrhmanhassanmohamed@gmail.com>
2024-09-16 16:29:27 +0300

    Make Delete & Toggle .

--- app/Livewire/TodoListComponent.php
+++ app/Livewire/TodoListComponent.php
@@ -11,22 +11,30 @@ class TodoListComponent extends Component
     use WithPagination;
     public $name;
     public $search;
-    public function create(){
-        // validate
+    public function create()
+    {
         $validated = $this->validate([
-            'name'=>['required','min:2','max:50']
+            'name' => ['required', 'min:2', 'max:50']
         ]);
-        // create the todo
         Todo::create($validated);
-        // clear the input
         $this->reset('name');
-        // send flash message
-        session()->flash('success','Created.');
+        session()->flash('success', 'Created.');
+    }
+    public function delete($todoId)
+    {
+        Todo::find($todoId)->delete();
+    }
+    public function toggle($todoId)
+    {
+        $todo = Todo::find($todoId);
+        $todo->update([
+            'completed' => !$todo->completed
+        ]);
     }
     public function render()
     {
-        return view('livewire.todo-list-component',[
-            'todos' => Todo::latest()->where('name','LIKE',"%{$this->search}%")->paginate(5)
+        return view('livewire.todo-list-component', [
+            'todos' => Todo::latest()->where('name', 'LIKE', "%{$this->search}%")->paginate(5)
         ]);
     }
 }
