decbdfac81a5950bc40a25a83ab16a8c5ee7f027
Abdo3297 <abdelrhmanhassanmohamed@gmail.com>
2024-09-17 01:33:13 +0300

    Make Edit.

--- app/Livewire/TodoListComponent.php
+++ app/Livewire/TodoListComponent.php
@@ -3,6 +3,7 @@
 namespace App\Livewire;
 
 use App\Models\Todo;
+use Exception;
 use Livewire\Component;
 use Livewire\WithPagination;
 
@@ -11,6 +12,8 @@ class TodoListComponent extends Component
     use WithPagination;
     public $name;
     public $search;
+    public $editingTodoID;
+    public $editingTodoName;
     public function create()
     {
         $validated = $this->validate([
@@ -19,10 +22,17 @@ class TodoListComponent extends Component
         Todo::create($validated);
         $this->reset('name');
         session()->flash('success', 'Created.');
+        $this->resetPage();
     }
     public function delete($todoId)
     {
-        Todo::find($todoId)->delete();
+        try {
+            Todo::findOrFail($todoId)->delete();
+        } catch(Exception $e) {
+            session()->flash('error', 'Failed to delete todo!');
+            return;
+        }
+        
     }
     public function toggle($todoId)
     {
@@ -30,6 +40,25 @@ class TodoListComponent extends Component
         $todo->update([
             'completed' => !$todo->completed
         ]);
+    }
+    public function edit($todoId)
+    {
+        $this->editingTodoID = $todoId;
+        $this->editingTodoName = Todo::find($todoId)->name;
+    }
+    public function cancelEdit()
+    {
+        $this->reset('editingTodoID','editingTodoName');
+    }
+    public function update()
+    {
+        $validated = $this->validate([
+            'editingTodoName' => ['required', 'min:2', 'max:50']
+        ]);
+        Todo::find($this->editingTodoID)->update([
+            'name' => $validated['editingTodoName']
+        ]);
+        $this->cancelEdit();
     }
     public function render()
     {
